85ffef140e68c7a7d00412d2d244bee157952ee2
Maximillian Kirchoff <max@lamasix.com>
2020-07-30 14:51:20 -0500

    tommy boy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 printf '\n\n Syncing to S3 from local "public" folder.\n Excluding any dot files or folders.\n\n'
-aws s3 sync app/ s3://sexfuckmaster.biz --acl public-read --exclude=".*" --delete --size-only
+aws s3 sync app/ s3://www.sexfuckmaster.biz --acl public-read --exclude=".*" --delete 
